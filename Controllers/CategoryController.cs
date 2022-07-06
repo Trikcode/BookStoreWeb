f500defddfa4c84dd5bbeebca8a18c3e15c774bc
Trikcode <ayesiganobert@gmail.com>
2022-07-06 16:26:58 +0300

    edit update and delete

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -28,7 +28,13 @@ namespace BookStoreWeb.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create( Category obj)
+
         {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The display order can't match the Name");
+                //ModelState.AddModelError("Name", "The display order can't match the Name");
+            }
             if (ModelState.IsValid)
             {
             _db.Categories.Add(obj);
@@ -36,6 +42,80 @@ namespace BookStoreWeb.Controllers
             return RedirectToAction("Index");
             //if in diff controller
             //return RedirectToAction("Index", another controller);
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryToDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFromDb);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The display order can't match the Name");
+                //ModelState.AddModelError("Name", "The display order can't match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //if in diff controller
+                //return RedirectToAction("Index", another controller);
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryToDb = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+
+            }
+            return View(categoryFromDb);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The display order can't match the Name");
+                //ModelState.AddModelError("Name", "The display order can't match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+                //if in diff controller
+                //return RedirectToAction("Index", another controller);
             }
             return View();
         }
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookStoreWeb.Models
 {
@@ -11,6 +12,8 @@ namespace BookStoreWeb.Models
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public string? Description { get; set; }
 
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Values should btn 1 and 100!!")]
         public int DisplayOrder { get; set; }
 
     }
